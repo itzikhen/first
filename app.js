2509a70008332f785a4d1fda092a537ea8003a76
Itzik Hen <itzikhen1@gmail.com>
2018-10-08 18:55:17 +0300

    itzik first heroku app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req,res){
-  res.send(`<h1>Welcome ITZIK to our first app</h1><h2>some other header,/h2>`)
+  res.send(`<h1>Welcome ITZIK to our first app</h1><h2>some other header</h2>`)
 });
 
 app.listen(process.env.PORT||3500, function () {
